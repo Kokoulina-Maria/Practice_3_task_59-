55fe930984f44647162df6c9c2f7c7900ce21afa
Maria <Maria@LAPTOP-N156UH30>
2017-08-13 13:04:56 +0500

    Добавьте файлы проекта.

---
+++ Practice_3_task_59е/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_3_task_59е
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
