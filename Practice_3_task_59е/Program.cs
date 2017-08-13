bdda1a11295ae52a3ac2754ff21e4872a4d85d4b
Maria <Maria@LAPTOP-N156UH30>
2017-08-13 14:47:55 +0500

    Программа работает для рациональных чисел x, y

--- Practice_3_task_59е/Program.cs
+++ Practice_3_task_59е/Program.cs
@@ -8,8 +8,29 @@ namespace Practice_3_task_59е
 {
     class Program
     {
+        static Double ReadDouble(string msg)
+        {// Ввод координаты с проверкой
+            Double number; bool ok;// переменная для проверки
+            do
+            {
+                Console.Write("Введите координату "+msg+" с точностью не более 15 знаков после запятой: ");
+                ok = Double.TryParse(Console.ReadLine(), out number);
+                if (!ok) Console.WriteLine("Неверный ввод!");
+            } while (!ok);// конец проверки
+            return (number);
+        }
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Double x = ReadDouble("x");
+                Double y = ReadDouble("y");
+                if (((x < 0) & (((y >= 0) & (x / 2 + 1 >=y)) || ((y < 0) & (-x / 2 - 1 <=y)))) || ((x >=0) & (x * x + y * y <= 1)))
+                    Console.WriteLine("Принадлежит");
+                else Console.WriteLine("Не принадлежит");
+                Console.ReadLine();               
+            }
         }
     }
 }
